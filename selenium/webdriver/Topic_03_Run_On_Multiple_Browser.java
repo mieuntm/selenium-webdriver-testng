cd05e31fa34a790531f15ffb4b9d13a16c034496
my mieu <mymieu95@gmail.com>
2021-12-18 12:15:20 +0700

    commit

---
+++ selenium/webdriver/Topic_03_Run_On_Multiple_Browser.java
@@ -0,0 +1,46 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_03_Run_On_Multiple_Browser {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+
+
+	@Test
+	public void TC_01_Firefox() {
+		System.setProperty("webdriver.firefox.driver", projectPath + "\\browserDrivers\\firefoxdriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void TC_02_Chrome() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void TC_03_Edge() {
+		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+		
+		driver.quit();
+		
+	}	
+}
